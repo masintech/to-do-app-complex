ef7e3774ea6d6ccce3d0b63366387653a3d7303f
weijeh <weijeh@avery-design.com.tw>
2021-05-07 16:09:12 +0800

    [Router]: add router as feature Project: Note   :

--- app.js
+++ app.js
@@ -2,15 +2,14 @@ const express = require('express')
 const app = express()
 
 
+const router = require('./router')
+
 app.use(express.static('public'))
 // second views is golder name
 app.set('views', 'views')
 // using ejs engine
 app.set('view engine', 'ejs')
 
-app.get('/', function (req, res) {
-    res.render('home-guest')
-})
-
+app.use('/', router)
 
 app.listen(3000)
