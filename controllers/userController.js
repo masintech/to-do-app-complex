911e1eba8f9bb7f9ddc3dd871965b3873f6e4ced
weijeh <weijeh@avery-design.com.tw>
2021-05-07 17:08:16 +0800

    model]

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,5 @@
+const User = require('../models/User')
+
 exports.login = function () {
 
 }
@@ -7,10 +9,11 @@ exports.logout = function () {
 }
 
 exports.register = function (req, res) {
+    let user = new User(req.body)
+    user.register()
     res.send('Please register')
 }
 
 exports.home = function (req, res) {
     res.render('home-guest')
-
 }
